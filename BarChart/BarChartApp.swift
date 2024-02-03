9a8468c16ad2ba9ea1c0e9a4472dededf70afe14
Franck <157825093+codeWithFranck@users.noreply.github.com>
2024-02-03 12:37:47 -0500

    Initial Commit

---
+++ BarChart/BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartApp.swift
+//  BarChart
+//
+//  Created by Franck Dalin Mbeutcha Tchuisseu on 2024-02-03.
+//
+
+import SwiftUI
+
+@main
+struct BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
