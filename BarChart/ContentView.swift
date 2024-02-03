9c89c09bccc9f8ca1d989513fc2914081310f68d
Franck <157825093+codeWithFranck@users.noreply.github.com>
2024-02-03 13:14:41 -0500

    Add model

--- BarChart/ContentView.swift
+++ BarChart/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import Charts
+
+struct SleepData: Identifiable {
+    let id = UUID()
+    let date: Date
+    let hoursOfSleep: Double
+    
+    init(date: String, hoursOfSleep: Double) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        
+        self.date = formatter.date(from: date) ?? Date.distantPast
+        self.hoursOfSleep = hoursOfSleep
+    }
+}
 
 struct ContentView: View {
+    let sleepDataWeek: [SleepData] = [
+        SleepData(date: "20240101", hoursOfSleep: 6.5),
+        SleepData(date: "20240102", hoursOfSleep: 7.0),
+        SleepData(date: "20240103", hoursOfSleep: 8.0),
+        SleepData(date: "20240104", hoursOfSleep: 6.0),
+        SleepData(date: "20240105", hoursOfSleep: 7.5),
+        SleepData(date: "20240106", hoursOfSleep: 5.5),
+        SleepData(date: "20240107", hoursOfSleep: 8.5)
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart(sleepDataWeek){
+            BarMark(
+                x: .value("Date", $0.date),
+                y: .value("Hours of Sleep", $0.hoursOfSleep)
+            )
         }
-        .padding()
     }
 }
 
